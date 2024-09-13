45df27307c29f48d1c4aea9f73659c12f1f71e03
retoheusser <reto.heusser@starmind.com>
2024-09-13 21:17:37 +0200

    more style

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -5,12 +5,23 @@ const { value } = defineProps<{ value: BlogPostItem }>()
 </script>
 
 <template>
-  <v-sheet>
-    <div>{{ value.title }}</div>
+  <v-sheet
+    color="amber-lighten-5"
+    class="my-8 pa-4"
+  >
+    <div class="text-h5 font-weight-bold mb-4">
+      {{ value.title }}
+    </div>
     <div
       @click="e => $emit('click', e)"
       v-html="value.content"
     />
-    <v-divider />
+    <v-lazy>
+      <!-- invisibly loading the real blogpost page counts as a view on Blogger -->
+      <iframe
+        class="d-none"
+        :src="value.url"
+      />
+    </v-lazy>
   </v-sheet>
 </template>
