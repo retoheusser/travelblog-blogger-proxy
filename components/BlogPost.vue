42e450a9bb4ee98455053b2ca1cd9c62bbabe769
retoheusser <reto.heusser@starmind.com>
2024-09-13 21:22:36 +0200

    clearer typing

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
 import type { BlogPostItem } from '../types/blogger.types'
 
-const { value } = defineProps<{ value: BlogPostItem }>()
+const props = defineProps<{ value: BlogPostItem }>()
+defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>()
 </script>
 
 <template>
@@ -10,17 +13,17 @@ const { value } = defineProps<{ value: BlogPostItem }>()
     class="my-8 pa-4"
   >
     <div class="text-h5 font-weight-bold mb-4">
-      {{ value.title }}
+      {{ props.value.title }}
     </div>
     <div
       @click="e => $emit('click', e)"
-      v-html="value.content"
+      v-html="props.value.content"
     />
     <v-lazy>
       <!-- invisibly loading the real blogpost page counts as a view on Blogger -->
       <iframe
         class="d-none"
-        :src="value.url"
+        :src="props.value.url"
       />
     </v-lazy>
   </v-sheet>
