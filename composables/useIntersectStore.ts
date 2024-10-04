5fa825cbe5a7298d7130cd156639a5b2e8870680
retoheusser <reto.heusser@starmind.com>
2024-10-04 19:27:39 +0200

    todo

--- composables/useIntersectStore.ts
+++ composables/useIntersectStore.ts
@@ -1,5 +1,7 @@
 export const useIntersectStore = defineStore('intersect', () => {
   const currentlyVisibleTitle = ref('home')
 
+  // TODO: store array of visible titles and display the topmost
+
   return { currentlyVisibleTitle }
 })
